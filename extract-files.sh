LineageOS/android_device_google_taimen

taimen: libshims: Add shim for camera blobs
Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
Change-Id: I5e622e68422f5c4da13c0752a7eb3f4b20ddae49

extract-files.sh
@@ -59,6 +59,11 @@ function blob_fixup() {
     product/etc/sysconfig/nexus.xml)
         sed -i 's/qulacomm/qualcomm/' "${2}"
         ;;
+    system/lib/lib-imsvideocodec.so | system/lib/libimsmedia_jni.so | system/lib64/lib-imsvideocodec.so | system/lib64/libimsmedia_jni.so)
+        for LIBGUI_SHIM in $(grep -L "libgui_shim.so" "${2}"); do
+            "${PATCHELF}" --add-needed "libgui_shim.so" "${LIBGUI_SHIM}"
+        done
+        ;;
     esac
 }
 

elginsk8r/android_device_oneplus_sm4350-common

extract-files.sh
@@ -60,9 +60,10 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        system_ext/lib64/lib-imsvideocodec.so)
-            for shim in $(grep -L "lib-imsvtshim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "lib-imsvtshim.so" "$shim"
+        # Fix missing symbols for IMS/Camera
+        system_ext/lib/lib-imsvideocodec.so | system_ext/lib/libimsmedia_jni.so | system_ext/lib64/lib-imsvideocodec.so | system_ext/lib64/libimsmedia_jni.so)
+            for shim in $(grep -L "libgui_shim.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libgui_shim.so" "${shim}"
             done
             ;;
         system_ext/lib64/libwfdnative.so)