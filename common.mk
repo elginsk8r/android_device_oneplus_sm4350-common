480d4b22bb2811e94ff6c18620b8073525816b27
Nick Reuter <nreuter85@gmail.com>
2022-06-14 20:50:58 -0500

    Revert "sm4350-common: remove touchscreen gestures for now"
    This reverts commit 2bf6078968531ec838ff8ebf0d04e1c9082a3390.
    
    Change-Id: I88034b94ee8fceb68b1426e147e28d380d0f2aa7

--- common.mk
+++ common.mk
@@ -448,6 +448,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.evervolv.touch@1.0-service.oneplus
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
